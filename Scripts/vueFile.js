cb2422d07b0fe7da6641ba2389c90793949116ff
Lukas Urba <lukas.urba@gmail.com>
2021-11-12 21:26:12 +0000

    Added basics for 1 lesson

---
+++ Scripts/vueFile.js
@@ -0,0 +1,23 @@
+
+    var webstore = new Vue({
+        el: '#app',
+        data: {
+            sitename: 'Vue.js Header',
+            cart: [],
+            product: {
+                id: 1001,
+                name: "Maths",
+                location: "London",
+                price: 50,
+                image: "Images/math.png",
+                spaces: 5,
+                btn: "Add to cart"
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        }
+    });
