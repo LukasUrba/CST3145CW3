fe7583711795958d00ee838b998cb3f318c99797
Lukas Urba <lukas.urba@gmail.com>
2022-04-06 18:01:56 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>{{sitename}}</h1>
+      <button @click="showCheckout">{{this.cart.length}} Checkout</button>
+    </header>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+
+  </div>
+</template>
+
+<script>
+// import Lessons from "./components/Lessons.vue";
+// import Checkout from "./components/Lessons.vue";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      sitename: "Vue.js Lessons",
+      cart: []
+    }
+  },
+  methods: {
+    showCheckout() {
+    }
+  },
+  // components: {
+  //   Lessons
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,111 @@
+<template>
+    <main>
+        <div v-for="product in searchField" :key="product.id">
+            <figure>
+                <img class="lessonImage" v-bind:src="product.image"/>
+            </figure>
+            <h2>Subject: {{product.name}}</h2>
+            <p>Location: {{product.location}}</p>
+            <p>Price: £{{product.price}}</p>
+            <p>Spaces available: {{product.spaces}}</p>
+            <button v-html="lessonButton" @click="addToCart(product)" v-if="canAdd(product)"></button>
+            <button v-html="lessonButton" disabled v-else></button>
+        </div>
+    </main>
+</template>
+<script>
+    export default {
+        name: "Product list",
+        data() {
+            return {
+                products: [
+                    {
+                        id: 1001,
+                        name: "Maths",
+                        location: "London",
+                        price: 50,
+                        image: "math.png",
+                        spaces: 5,
+                    },
+                    {
+                        id: 1002,
+                        name: "English",
+                        location: "Birmingham",
+                        price: 40,
+                        image: "english.png",
+                        spaces: 5,
+                    },
+                    {
+                        id: 1003,
+                        name: "Physics",
+                        location: "Enfield",
+                        price: 45,
+                        image: "science.jpeg",
+                        spaces: 5,
+                    },
+                    {
+                        id: 1004,
+                        name: "Chemistry",
+                        location: "Birmingham",
+                        price: 50,
+                        image: "science.jpeg",
+                        spaces: 5,
+                    },
+                    {
+                        id: 1005,
+                        name: "French",
+                        location: "Brighton",
+                        price: 45,
+                        image: "french.jpeg",
+                        spaces: 5,
+                    },
+                    {
+                        id: 1006,
+                        name: "Geography",
+                        location: "London",
+                        price: 50,
+                        image: "geography.jpeg",
+                        spaces: 5,
+                    },
+                    {
+                        id: 1007,
+                        name: "Statistics",
+                        location: "Basildon",
+                        price: 50,
+                        image: "math.png",
+                        spaces: 5,
+                    },
+                    {
+                        id: 1008,
+                        name: "IT",
+                        location: "London",
+                        price: 40,
+                        image: "IT.png",
+                        spaces: 5,
+                    },
+                    {
+                        id: 1009,
+                        name: "History",
+                        location: "Bournemouth",
+                        price: 40,
+                        image: "history.jpeg",
+                        spaces: 5,
+                    },
+                    {
+                        id: 1010,
+                        name: "Biology",
+                        location: "Birmingham",
+                        price: 45,
+                        image: "science.jpeg",
+                        spaces: 5,
+                    }
+                ]
+            }
+        },
+        methods: {
+            addToCart(product) {
+                this.$emit('addToCart',product);
+            }
+        }
+    }
+</script>
