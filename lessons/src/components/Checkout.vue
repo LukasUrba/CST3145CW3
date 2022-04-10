960ee9c261441c657a78a2fe73a7b25bdcb6ba42
Lukas Urba <lukas.urba@gmail.com>
2022-04-10 17:28:56 +0100

    Added remove functionality

--- lessons/src/components/Checkout.vue
+++ lessons/src/components/Checkout.vue
@@ -3,27 +3,31 @@
     <div>
         <h2>Shopping Cart</h2>
         <div v-for='product in cart' :key="product.id">
-            {{product.name}} <button>Remove</button>
+            {{product.name}} <button @click="removeProduct(product)">Remove</button>
         </div>
         <p>
             <strong>First Name: </strong> <input v-model="fName" />
             <strong>Last Name: </strong> <input v-model="lName" />
             <strong>Phone Number: </strong> <input v-model="phoneNo" />
             <button>Place Order</button>
-            
         </p>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Checkout",
+        name: "CheckOut",
         props: ['cart'],
         data() {
             return {
                 fName: "",
                 lName: "",
                 phoneNo: ""
+            }
+        },
+        methods: {
+            removeProduct(product) {
+                this.$emit('removeProduct', product)
             }
         }
     }
