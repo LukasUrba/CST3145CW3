0664f59edfccaede657ccfaf1e5861cb52e12a95
Lukas Urba <lukas.urba@gmail.com>
2022-04-10 17:04:08 +0100

    Created Checkout Page

--- lessons/src/components/Checkout.vue
+++ lessons/src/components/Checkout.vue
@@ -1,3 +1,19 @@
+
+<template>
+    <div>
+        <h2>Shopping Cart</h2>
+        <div v-for='product in cart' :key="product.id">
+            {{product.name}} <button>Remove</button>
+        </div>
+        <p>
+            <strong>First Name: </strong> <input v-model="fName" />
+            <strong>Last Name: </strong> <input v-model="lName" />
+            <strong>Phone Number: </strong> <input v-model="phoneNo" />
+            <button>Place Order</button>
+        </p>
+    </div>
+</template>
+
 <script>
     export default {
         name: "Checkout",
